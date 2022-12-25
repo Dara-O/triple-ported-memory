f67c5ce029ada14f8645a32790ff03e8474b09bb
Isaac Ogunmola <daraogunmola2@gmail.com>
2022-12-25 04:16:17 -0500

    fixed routing logic

--- src/port_deprioritizer.v
+++ src/port_deprioritizer.v
@@ -2,6 +2,9 @@
 
 `include "shared_params.vh"
 
+/* 
+    NOTE: The module assumes that port1_orig_id != port2_orig_id != port3_orig_id (unless they are all 0) 
+*/
 module port_deprioritizer #(parameter WIDTH = 8) (
     input   wire    [1:0]           port1_orig_id,
     input   wire    [1:0]           port2_orig_id,
@@ -16,86 +19,204 @@ module port_deprioritizer #(parameter WIDTH = 8) (
     input   wire                    port3_valid_in,
 
 
-    output  reg     [WIDTH-1:0]     port1_data_out,
-    output  reg     [WIDTH-1:0]     port2_data_out,
-    output  reg     [WIDTH-1:0]     port3_data_out,
+    output  wire    [WIDTH-1:0]     port1_data_out,
+    output  wire    [WIDTH-1:0]     port2_data_out,
+    output  wire    [WIDTH-1:0]     port3_data_out,
 
-    output  reg                     port1_valid_out,
-    output  reg                     port2_valid_out,
-    output  reg                     port3_valid_out
+    output  wire                    port1_valid_out,
+    output  wire                    port2_valid_out,
+    output  wire                    port3_valid_out
 );
 
-    always @ (*) begin
-        case ({port1_orig_id, port2_orig_id, port3_orig_id})
-            {ORIG_PORT_1_ID, ORIG_PORT_2_ID, ORIG_PORT_3_ID} : begin
-                port1_data_out  <=  port1_data_in;
-                port2_data_out  <=  port2_data_in;
-                port3_data_out  <=  port3_data_in;
+    reg [WIDTH-1:0] pre_port1_data_from_pid1; // port1 driven by port1_orig_id
+    reg [WIDTH-1:0] pre_port2_data_from_pid1; // port2 driven by port1_orig_id
+    reg [WIDTH-1:0] pre_port3_data_from_pid1; // port3 driven by port1_orig_id
+    reg pre_port1_valid_from_pid1;
+    reg pre_port2_valid_from_pid1;
+    reg pre_port3_valid_from_pid1;
 
-                port1_valid_out <=  port1_valid_in;
-                port2_valid_out <=  port2_valid_in;
-                port3_valid_out <=  port3_valid_in;
-            end 
-            
-            {ORIG_PORT_1_ID, ORIG_PORT_3_ID, ORIG_PORT_2_ID} : begin
-                port1_data_out  <=  port1_data_in;
-                port2_data_out  <=  port3_data_in;
-                port3_data_out  <=  port2_data_in;
+    always @(*) begin
+        case (port1_orig_id) 
+        ORIG_PORT_1_ID : begin
+            pre_port1_data_from_pid1    = port1_data_in;
+            pre_port2_data_from_pid1    = 0;
+            pre_port3_data_from_pid1    = 0;
 
-                port1_valid_out <=  port1_valid_in;
-                port2_valid_out <=  port3_valid_in;
-                port3_valid_out <=  port2_valid_in;
-            end
+            pre_port1_valid_from_pid1   = port1_valid_in;
+            pre_port2_valid_from_pid1   = 0;
+            pre_port3_valid_from_pid1   = 0;
+        end 
 
-            {ORIG_PORT_2_ID, ORIG_PORT_1_ID, ORIG_PORT_3_ID} : begin
-                port1_data_out  <=  port2_data_in;
-                port2_data_out  <=  port1_data_in;
-                port3_data_out  <=  port3_data_in;
+        ORIG_PORT_2_ID : begin
+            pre_port1_data_from_pid1    = 0;
+            pre_port2_data_from_pid1    = port1_data_in;
+            pre_port3_data_from_pid1    = 0;
 
-                port1_valid_out <=  port2_valid_in;
-                port2_valid_out <=  port1_valid_in;
-                port3_valid_out <=  port3_valid_in;
-            end
+            pre_port1_valid_from_pid1   = 0;
+            pre_port2_valid_from_pid1   = port1_valid_in;
+            pre_port3_valid_from_pid1   = 0;
+        end 
 
-            {ORIG_PORT_2_ID, ORIG_PORT_3_ID, ORIG_PORT_1_ID} : begin
-                port1_data_out  <=  port2_data_in;
-                port2_data_out  <=  port3_data_in;
-                port3_data_out  <=  port1_data_in;
+        ORIG_PORT_3_ID : begin
+            pre_port1_data_from_pid1    = 0;
+            pre_port2_data_from_pid1    = 0;
+            pre_port3_data_from_pid1    = port1_data_in;
 
-                port1_valid_out <=  port2_valid_in;
-                port2_valid_out <=  port3_valid_in;
-                port3_valid_out <=  port1_valid_in;
-            end
+            pre_port1_valid_from_pid1   = 0;
+            pre_port2_valid_from_pid1   = 0;
+            pre_port3_valid_from_pid1   = port1_valid_in;
+        end 
 
-            {ORIG_PORT_3_ID, ORIG_PORT_1_ID, ORIG_PORT_2_ID} : begin
-                port1_data_out  <=  port3_data_in;
-                port2_data_out  <=  port1_data_in;
-                port3_data_out  <=  port2_data_in;
+        ORIG_PORT_INVALID_ID : begin
+            pre_port1_data_from_pid1    = 0;
+            pre_port2_data_from_pid1    = 0;
+            pre_port3_data_from_pid1    = 0;
 
-                port1_valid_out <=  port3_valid_in;
-                port2_valid_out <=  port1_valid_in;
-                port3_valid_out <=  port2_valid_in;
-            end
+            pre_port1_valid_from_pid1   = 0;
+            pre_port2_valid_from_pid1   = 0;
+            pre_port3_valid_from_pid1   = 0;
+        end 
 
-            {ORIG_PORT_3_ID, ORIG_PORT_2_ID, ORIG_PORT_1_ID} : begin
-                port1_data_out  <=  port3_data_in;
-                port2_data_out  <=  port2_data_in;
-                port3_data_out  <=  port1_data_in;
+        default : begin
+            pre_port1_data_from_pid1    = 0;
+            pre_port2_data_from_pid1    = 0;
+            pre_port3_data_from_pid1    = 0;
 
-                port1_valid_out <=  port3_valid_in;
-                port2_valid_out <=  port2_valid_in;
-                port3_valid_out <=  port1_valid_in;
-            end
-
-            default: begin
-                port1_data_out  <= 0;
-                port2_data_out  <= 0;
-                port3_data_out  <= 0;
-                port1_valid_out <= 0;
-                port2_valid_out <= 0;
-                port3_valid_out <= 0;
-            end
+            pre_port1_valid_from_pid1   = 0;
+            pre_port2_valid_from_pid1   = 0;
+            pre_port3_valid_from_pid1   = 0;
+        end
         endcase
     end
+
+    reg [WIDTH-1:0] pre_port1_data_from_pid2; // port1 driven by port2_orig_id
+    reg [WIDTH-1:0] pre_port2_data_from_pid2; // port2 driven by port2_orig_id
+    reg [WIDTH-1:0] pre_port3_data_from_pid2; // port3 driven by port2_orig_id
+    reg pre_port1_valid_from_pid2;
+    reg pre_port2_valid_from_pid2;
+    reg pre_port3_valid_from_pid2;
+
+    always @(*) begin
+        case (port2_orig_id) 
+        ORIG_PORT_1_ID : begin
+            pre_port1_data_from_pid2    = port2_data_in;
+            pre_port2_data_from_pid2    = 0;
+            pre_port3_data_from_pid2    = 0;
+
+            pre_port1_valid_from_pid2   = port2_valid_in;
+            pre_port2_valid_from_pid2   = 0;
+            pre_port3_valid_from_pid2   = 0;
+        end 
+
+        ORIG_PORT_2_ID : begin
+            pre_port1_data_from_pid2    = 0;
+            pre_port2_data_from_pid2    = port2_data_in;
+            pre_port3_data_from_pid2    = 0;
+
+            pre_port1_valid_from_pid2   = 0;
+            pre_port2_valid_from_pid2   = port2_valid_in;
+            pre_port3_valid_from_pid2   = 0;
+        end 
+
+        ORIG_PORT_3_ID : begin
+            pre_port1_data_from_pid2    = 0;
+            pre_port2_data_from_pid2    = 0;
+            pre_port3_data_from_pid2    = port2_data_in;
+
+            pre_port1_valid_from_pid2   = 0;
+            pre_port2_valid_from_pid2   = 0;
+            pre_port3_valid_from_pid2   = port2_valid_in;
+        end 
+
+        ORIG_PORT_INVALID_ID : begin
+            pre_port1_data_from_pid2    = 0;
+            pre_port2_data_from_pid2    = 0;
+            pre_port3_data_from_pid2    = 0;
+
+            pre_port1_valid_from_pid2   = 0;
+            pre_port2_valid_from_pid2   = 0;
+            pre_port3_valid_from_pid2   = 0;
+        end 
+
+        default : begin
+            pre_port1_data_from_pid2    = 0;
+            pre_port2_data_from_pid2    = 0;
+            pre_port3_data_from_pid2    = 0;
+
+            pre_port1_valid_from_pid2   = 0;
+            pre_port2_valid_from_pid2   = 0;
+            pre_port3_valid_from_pid2   = 0;
+        end
+        endcase
+    end
+
+    reg [WIDTH-1:0] pre_port1_data_from_pid3; // port1 driven by port3_orig_id
+    reg [WIDTH-1:0] pre_port2_data_from_pid3; // port2 driven by port3_orig_id
+    reg [WIDTH-1:0] pre_port3_data_from_pid3; // port3 driven by port3_orig_id
+    reg pre_port1_valid_from_pid3;
+    reg pre_port2_valid_from_pid3;
+    reg pre_port3_valid_from_pid3;
+
+    always @(*) begin
+        case (port3_orig_id) 
+        ORIG_PORT_1_ID : begin
+            pre_port1_data_from_pid3    = port3_data_in;
+            pre_port2_data_from_pid3    = 0;
+            pre_port3_data_from_pid3    = 0;
+
+            pre_port1_valid_from_pid3   = port3_valid_in;
+            pre_port2_valid_from_pid3   = 0;
+            pre_port3_valid_from_pid3   = 0;
+        end 
+
+        ORIG_PORT_2_ID : begin
+            pre_port1_data_from_pid3    = 0;
+            pre_port2_data_from_pid3    = port3_data_in;
+            pre_port3_data_from_pid3    = 0;
+
+            pre_port1_valid_from_pid3   = 0;
+            pre_port2_valid_from_pid3   = port3_valid_in;
+            pre_port3_valid_from_pid3   = 0;
+        end 
+
+        ORIG_PORT_3_ID : begin
+            pre_port1_data_from_pid3    = 0;
+            pre_port2_data_from_pid3    = 0;
+            pre_port3_data_from_pid3    = port3_data_in;
+
+            pre_port1_valid_from_pid3   = 0;
+            pre_port2_valid_from_pid3   = 0;
+            pre_port3_valid_from_pid3   = port3_valid_in;
+        end 
+
+        ORIG_PORT_INVALID_ID : begin
+            pre_port1_data_from_pid3    = 0;
+            pre_port2_data_from_pid3    = 0;
+            pre_port3_data_from_pid3    = 0;
+
+            pre_port1_valid_from_pid3   = 0;
+            pre_port2_valid_from_pid3   = 0;
+            pre_port3_valid_from_pid3   = 0;
+        end 
+
+        default : begin
+            pre_port1_data_from_pid3    = 0;
+            pre_port2_data_from_pid3    = 0;
+            pre_port3_data_from_pid3    = 0;
+
+            pre_port1_valid_from_pid3   = 0;
+            pre_port2_valid_from_pid3   = 0;
+            pre_port3_valid_from_pid3   = 0;
+        end
+        endcase
+    end
+
+    assign port1_data_out = pre_port1_data_from_pid1 | pre_port1_data_from_pid2 | pre_port1_data_from_pid3;
+    assign port2_data_out = pre_port2_data_from_pid1 | pre_port2_data_from_pid2 | pre_port2_data_from_pid3; 
+    assign port3_data_out = pre_port3_data_from_pid1 | pre_port3_data_from_pid2 | pre_port3_data_from_pid3; 
+
+    assign port1_valid_out = pre_port1_valid_from_pid1 | pre_port1_valid_from_pid2 | pre_port1_valid_from_pid3;
+    assign port2_valid_out = pre_port2_valid_from_pid1 | pre_port2_valid_from_pid2 | pre_port2_valid_from_pid3;
+    assign port3_valid_out = pre_port3_valid_from_pid1 | pre_port3_valid_from_pid2 | pre_port3_valid_from_pid3;
 
 endmodule
